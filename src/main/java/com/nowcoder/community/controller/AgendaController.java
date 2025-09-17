59ade01698eaaa1813848c019634af35120c5a77
Cortalo <Cortalo@outlook.com>
2025-09-17 17:15:54 +0200

    using custom annotation for interceptor to prevent vising methods needs login

--- src/main/java/com/nowcoder/community/controller/AgendaController.java
+++ src/main/java/com/nowcoder/community/controller/AgendaController.java
@@ -1,5 +1,6 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.annotation.LoginRequired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AgendaController {
 
 
+    @LoginRequired
     @RequestMapping(path = "/add", method = RequestMethod.GET)
     public String getAddTaskPage() {
         return "/site/add-task";
     }
 
+    @LoginRequired
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     public String addTask(Model model, String title) {
         System.out.println("title is: " + title);
