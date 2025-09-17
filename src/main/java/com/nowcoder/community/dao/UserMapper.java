fd6af0bba71980526e60dee13398a6a6b15be1c3
Cortalo <Cortalo@outlook.com>
2025-09-17 14:16:49 +0200

    setup register function (html page and post form, register service and sending email...)

---
+++ src/main/java/com/nowcoder/community/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    User selectById(int id);
+
+    User selectByUsername(String username);
+
+    User selectByEmail(String email);
+
+    int insertUser(User user);
+
+}
