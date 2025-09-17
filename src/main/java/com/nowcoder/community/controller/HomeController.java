19900d39d92abdd05a512f28a983b563f59aa365
Cortalo <Cortalo@outlook.com>
2025-09-17 09:48:50 +0200

    show the list of this week in the index page

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.DayOfWeek;
 import java.util.*;
 
 @Controller
@@ -12,13 +15,24 @@ public class HomeController {
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getIndexPage(Model model) {
-        Date today = new Date();
-        List<Map<String, Object>> discussPosts = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            Map<String, Object> map = new HashMap<>();
-            discussPosts.add(map);
+        LocalDate today = LocalDate.now();
+
+        // Get a list of dates for this week (Monday to Sunday)
+        LocalDate monday = today.with(DayOfWeek.MONDAY);
+        List<LocalDate> weekDates = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            weekDates.add(monday.plusDays(i));
         }
-        model.addAttribute("discussPosts", discussPosts);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy");
+
+        List<Map<String, Object>> days = new ArrayList<>();
+        for (LocalDate date : weekDates) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("date", date);
+            map.put("formattedDate", date.format(formatter));
+            days.add(map);
+        }
+        model.addAttribute("days", days);
         return "/index";
     }
 
