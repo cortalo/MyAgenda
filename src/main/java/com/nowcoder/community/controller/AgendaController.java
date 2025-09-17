8b2a7c6d41b9974c359ccca76642ce2b149ef79d
Cortalo <Cortalo@outlook.com>
2025-09-17 17:26:06 +0200

    receive the post form add-task.html to controller

--- src/main/java/com/nowcoder/community/controller/AgendaController.java
+++ src/main/java/com/nowcoder/community/controller/AgendaController.java
@@ -1,6 +1,9 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.entity.AgendaEntry;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/agenda")
 public class AgendaController {
+
+    @Autowired
+    HostHolder hostHolder;
 
 
     @LoginRequired
@@ -19,8 +25,11 @@ public class AgendaController {
 
     @LoginRequired
     @RequestMapping(path = "/add", method = RequestMethod.POST)
-    public String addTask(Model model, String title) {
-        System.out.println("title is: " + title);
+    public String addTask(Model model, AgendaEntry agendaEntry, String startTimeString, String endTimeString) {
+        agendaEntry.setUserId(hostHolder.getUser().getId());
+        System.out.println(agendaEntry);
+        System.out.println(startTimeString);
+        System.out.println(endTimeString);
         return "redirect:/index";
     }
 
